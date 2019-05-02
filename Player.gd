812c79d054bec73854c6c13ddad25434c7ea39c1
SantiagoGerola <48567898+SantiagoGerola@users.noreply.github.com>
2019-05-02 15:35:55 -0300

    refinamiento-del-scrip-player

--- Player.gd
+++ Player.gd
@@ -1,17 +1,19 @@
 extends KinematicBody2D
 
-var p_velocity = Vector2(0,0)
+var p_velocity = Vector2(15,0) # Otorga movimiento inicial
 
 func _physics_process(delta):
-	getMove()
-	move_and_slide(p_velocity)
+    # Checkea si se presionaron teclas
+    get_move()
+    # Aplica las fisicas de movimiento
+    move_and_slide(p_velocity)
 
-func getMove():
-	if Input.is_action_pressed("ui_right"):
-		p_velocity.x += 10
-	if Input.is_action_pressed("ui_left"):
-		p_velocity.x -= 10
-	if Input.is_action_pressed("ui_up"):
-		p_velocity.y -= 10
-	if Input.is_action_pressed("ui_down"):
-		p_velocity.y += 10
+func get_move():
+    if Input.is_action_pressed("ui_right"):
+        p_velocity.x += 15
+    if Input.is_action_pressed("ui_left"):
+        p_velocity.x -= 15
+    if Input.is_action_pressed("ui_up"):
+        p_velocity.y -= 15
+    if Input.is_action_pressed("ui_down"):
+        p_velocity.y += 15
